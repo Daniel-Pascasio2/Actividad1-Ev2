ed58d709c30a7f9e488098607bdb9a2bc5957235
AndresKenichi <oyakatasama.kai@gmail.com>
2020-09-05 21:07:34 -0600

    GODITO CAMBIO ACTIVIDAD 3

---
+++ app/src/main/java/com/example/actividad1_ev2/Capitales.java
@@ -0,0 +1,41 @@
+package com.example.actividad1_ev2;
+
+public class Capitales {
+
+    String Continentes;
+    String paises;
+    String capitalP;
+
+    public Capitales(String Continentes, String paises, String capitalP){
+        this.Continentes=Continentes;
+        this.capitalP=Continentes;
+        this.capitalP=capitalP;
+
+
+
+    }
+
+    public String getContinentes() {
+        return Continentes;
+    }
+
+    public void setContinentes(String continentes) {
+        Continentes = continentes;
+    }
+
+    public String getPaises() {
+        return paises;
+    }
+
+    public void setPaises(String paises) {
+        this.paises = paises;
+    }
+
+    public String getCapitalP() {
+        return capitalP;
+    }
+
+    public void setCapitalP(String capitalP) {
+        this.capitalP = capitalP;
+    }
+}
