8cfb1b31eae5bdaaccdb6b87af94f88cf722ca94
Daniel-Pascasio2 <joshepascasio.12@gmail.comm>
2020-09-05 20:52:16 -0600

    actualicé act3, redireccioné a act 41

--- app/src/main/java/com/example/actividad1_ev2/Actividad3.java
+++ app/src/main/java/com/example/actividad1_ev2/Actividad3.java
@@ -55,6 +55,7 @@ public class Actividad3 extends AppCompatActivity {
             btnSiguienteA4.setOnClickListener(new View.OnClickListener() {
                 //creo objeto para desplazarme a la activity4
                 Intent c = new Intent(Actividad3.this, Actividad4.class);
+                Intent cc = new Intent(Actividad3.this, Actividad41.class);
                 //variables booleanas que almacenaran si un rbLUGAR y rbOPCION se han seleccionado
                 boolean xt=false;
                 boolean yt=false;
@@ -75,34 +76,33 @@ public class Actividad3 extends AppCompatActivity {
                         }
                         //Los RadioButton de OPCIONES
                         if(rbRadioButton.isChecked()){
+
                             Sel=rbRadioButton.getText().toString();
-                            yt=true;
-                        }else if(rbCheckBox.isChecked()){
-                            Sel=rbCheckBox.getText().toString();
-                            yt=true;
-                        }
-                    //si algun rb LUGARES se selecciono, es true, sino muestra mensaje TOAST
-                    if(xt==true){
-                        //si algun rb OPCIONES se selecciono, es true, sino muestra mensaje TOAST
-                        if(yt==true)
-                        {
-                            //llamo al objeto intent "c", y con putExtra(name,value), envio al otro Activity dichos datos
-                            //En este caso son Continente,Lugar y Seleccion
                             continente=getIntent().getStringExtra("CONTINENTE");
+
                             c.putExtra("CONTINENTE",continente);
-                            c.putExtra("LUGAR",Lugar);//
-                            c.putExtra("OPCION",Sel);//Condicion
+                            c.putExtra("LUGAR",Lugar);
+                            c.putExtra("OPCION",Sel);
                             //Ahora iniciamos la proxima actividad declarada en "c"
                             startActivity(c);
                         }else{
+                            Mensajes("Seleccione un Lugar");
+                        }
 
+                        if(rbCheckBox.isChecked()){
+
+                            Sel=rbCheckBox.getText().toString();
+                            continente=getIntent().getStringExtra("CONTINENTE");
+                            c.putExtra("CONTINENTE",continente);
+                            c.putExtra("LUGAR",Lugar);
+                            c.putExtra("OPCION",Sel);
+                            //Ahora iniciamos la proxima actividad declarada en "cc"
+                            startActivity(cc);
+                        }else
+                        {
                             Mensajes("Seleccione un tipo de GROUP");
                         }
 
-                    }else
-                    {
-                        Mensajes("Seleccione un Lugar");
-                    }
                 }
             });
 
